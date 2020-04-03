0b726a83214126a57a5634bfd027b51569bccd7d
DuarteFelicio <duartefelicio1@hotmail.com>
2020-04-03 15:04:21 +0100

    demo 1

---
+++ WebApplication1/WebApplication1/ClientApp/src/components/UploadFile.js
@@ -0,0 +1,50 @@
+﻿import React from 'react';
+
+
+export class UploadFile extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { file: '', msg: '' };
+        this.onFileChange = this.onFileChange.bind(this);
+        this.uploadFileData = this.uploadFileData.bind(this);
+    }
+
+    onFileChange = (event) => {
+        this.setState({
+            file: event.target.files[0]
+        });
+    }
+
+    uploadFileData = (event) => {
+        event.preventDefault();
+        this.setState({ msg: '' });
+
+        let data = new FormData();
+        data.append('files', this.state.file);
+
+        fetch('api/filepreview', {
+            method: 'POST',
+            body: data
+        }).then(response => {
+            return response.text();
+            }).then(data => {
+            console.log(data)
+            this.setState({ msg: data });
+        });
+    }
+    
+
+    render() {
+        return (
+            <div id="container">
+                <h1>File Upload Example using React</h1>
+                <h3>Upload a File</h3>
+                <h4>{this.state.msg}</h4>
+                <input onChange={this.onFileChange} type="file"></input>
+                <button disabled={!this.state.file} onClick={this.uploadFileData}>Upload</button>
+            </div>
+        )
+    }
+
+}
---
+++ WebApplication1/WebApplication1/Model/FilePreview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationDemo
+{
+    public class FilePreview
+    {
+        public int NumCol { get; set; }
+
+        public int NumLine { get; set; }
+
+        public List<String> FirstLines { get; set; }
+    }
+}
