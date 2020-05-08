136978a7485d9836047654c833abe1f320547123
rubencafe <rubencafe@hotmail.com>
2020-05-08 17:11:30 +0100

    remaster upload.js

--- DataAnnotation/ClientApp/src/components/UploadFile.js
+++ DataAnnotation/ClientApp/src/components/UploadFile.js
@@ -22,12 +22,13 @@ export class UploadFile extends Component {
         this.handleClick = this.handleClick.bind(this)
         this.upload = this.upload.bind(this)
         this.removeFileUploading = this.removeFileUploading.bind(this)
+        this.handleProgress = this.handleProgress.bind(this)
     }
  
-    removeFileUploading(file) {
+    removeFileUploading(name) {
         var array = this.state.uploading
         var names = array.map(o => o.name)
-        var index = names.indexOf(file.name)
+        var index = names.indexOf(name)
         if (index !== -1) {
             array.splice(index, 1)
             this.setState({
@@ -47,25 +48,26 @@ export class UploadFile extends Component {
         this.uploadFromUrl(this.state.url)
     }
 
+    handleProgress(progressEvent, name) {
+        const { loaded, total } = progressEvent;
+        let percent = Math.floor((loaded * 100) / total)
+        console.log(`${loaded}kb of ${total}kb | ${percent}%`);
+
+        var array = this.state.uploading
+        var names = array.map(o => o.name)
+        var index = names.indexOf(name)
+        array[index].percentage = percent;
+        this.setState({ uploadPercentage: array })
+    }
+
     async upload(file) {
         let data = new FormData();
         const token = await authService.getAccessToken()
         data.append('files', file);
-
+        
         const options = {
             onUploadProgress: (progressEvent) => {
-                const { loaded, total } = progressEvent;
-                let percent = Math.floor((loaded * 100) / total)
-                console.log(`${loaded}kb of ${total}kb | ${percent}%`);
-
-                //if (percent < 100) {
-                    var array = this.state.uploading
-                    var names = array.map(o => o.name)
-                    var index = names.indexOf(file.name)
-                    array[index].percentage = percent;
-                    this.setState({ uploadPercentage: array })
-                    
-                //}
+                this.handleProgress(progressEvent, file.name)
             },
             headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
         }
@@ -73,49 +75,65 @@ export class UploadFile extends Component {
             uploading: this.state.uploading.concat(
                 {
                     name: file.name,
-                    percentage: 0
+                    percentage: 0,
+                    method: "local"
                 })
-           
         })
-
         axios.post("FileUpload/Physical", data, options)
             .then(res => {
                 setTimeout(() => {
-                    this.removeFileUploading(file)
+                    this.removeFileUploading(file.name)
                     this.setState({ accepted: this.state.accepted.concat(file) })
                 }, 1000)
             }).catch(err => {
-                this.removeFileUploading(file)
-                this.setState({ rejected: this.state.rejected.concat(file) })
+                setTimeout(() => {
+                    this.removeFileUploading(file.name)
+                    this.setState({ rejected: this.state.rejected.concat(file) })
+                }, 1000)
             })
     }
 
 
     async uploadFromUrl(url) {
         const token = await authService.getAccessToken()
-        const response = await fetch(`FileUpload/Remote?url=${url}`, {
-            method: 'POST',
-            headers: !token ? {} : { 'Authorization': `Bearer ${token}`, 'url': url }
-        })
-        const data = await response.json()
-        if (response.status == 201) {
-            this.setState({
-                accepted: this.state.accepted.concat([
-                    {
-                        name: data.name,
-                        size: data.size
-                    }])
+        var urlArray = url.split('/')
+        var name = urlArray[urlArray.length-1]
+        const options = {           
+            headers: !token ? {} : { 'Authorization': `Bearer ${token}` },
+            transformResponse: axios.defaults.transformResponse.concat((data) => {
+                return data
             })
         }
-        else {
-            this.setState({
-                rejected: this.state.rejected.concat([
-                    {
-                        name: data.key,
-                        size : ""
-                    }])
+        this.setState({
+            uploading: this.state.uploading.concat(
+                {
+                    name: name,
+                    method: "remote"
+                })
+        })
+        axios.post(`FileUpload/Remote?url=${url}`, null, options)
+            .then(response => {                
+                setTimeout(() => {
+                    this.removeFileUploading(name)
+                    this.setState({
+                        accepted: this.state.accepted.concat([
+                            {
+                                name: response.data.name,
+                                size: response.data.size
+                            }])
+                    })
+                }, 1000)
             })
-        }       
+            .catch(err => {
+                this.removeFileUploading(name)
+                this.setState({
+                    rejected: this.state.rejected.concat([
+                        {
+                            name: url,
+                            size: "0"
+                        }])
+                })
+            })
     }
 
 
@@ -150,7 +168,7 @@ export class UploadFile extends Component {
                         <input type="text" name="url" className="login-input" placeholder="URL to upload file" onChange={this.onChange.bind(this)} />
                         <button type="button" class="btn btn-outline-primary" onClick={this.handleClick}>Upload</button>
                     </div>
-                    <h4>Uploading files:</h4>
+                    {this.state.uploading.length>0 && <h4>Uploading files:</h4>
                     <ul>
                         {
                             this.state.uploading.map(o =>
@@ -164,7 +182,7 @@ export class UploadFile extends Component {
                                 </li>)
 
                         }
-                    </ul>
+                    </ul>}
                     <h4>Accepted files:</h4>
                     <ul>
                         {
