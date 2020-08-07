3015a94f6a7deda263e2040e79abda29a9d6ff3f
DuarteFelicio <duartefelicio1@hotmail.com>
2020-08-07 19:43:25 +0100

    fornetnedf

---
+++ DataAnnotation/ClientApp/src/components/AlertComp.js
@@ -0,0 +1,26 @@
+﻿import React from 'react'
+
+export const successMessage = (message) => (
+    <div class="alert alert-success alert-dismissible ">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {message}
+    </div>
+)
+export const warningMessage = (message) => (
+    <div class="alert alert-warning alert-dismissible ">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {message}
+    </div>
+)
+export const infoMessage = (message) => (
+    <div class="alert alert-info alert-dismissible ">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {message}
+    </div>
+)
+export const dangerMessage = (message) => (
+    <div class="alert alert-danger alert-dismissible ">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        {message}
+    </div>
+)
