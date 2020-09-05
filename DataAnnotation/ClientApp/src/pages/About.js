5581e7d91ac68fb2cc2fb2d286a586178b8a0b6d
DuarteFelicio <duartefelicio1@hotmail.com>
2020-09-05 22:50:37 +0100

    final touches

--- DataAnnotation/ClientApp/src/pages/About.js
+++ DataAnnotation/ClientApp/src/pages/About.js
@@ -2,6 +2,9 @@
 import analyseFileImage from '../assets/analysefile.png'
 import uploadFileImage from '../assets/uploadfile.png'
 import workspaceImage from '../assets/workspace.png'
+import duarteImage from '../assets/Duarte.jpg'
+import cafeImage from '../assets/cafe.jpg'
+import ivoImage from '../assets/ivo.jpg'
 import { Accordion, Card, Button, Carousel, Jumbotron } from 'react-bootstrap'
 import CardComp from '../components/CardComp'
 
@@ -18,7 +21,9 @@ export default class About extends Component {
             <div>
                 <div class="row" style={{ backgroundColor: "#F5F5F5", maxWidth: "1919px" }}>
                     <div class="col-8" style={{ padding: "100px 100px 100px 100px" }}>
-                        <img src={uploadFileImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                        <figure>
+                            <img src={uploadFileImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                         </figure>
                     </div>
                     <div class="col-4" style={{ padding: "150px 150px 100px 75px" }}>
                     <Jumbotron>
@@ -31,7 +36,7 @@ export default class About extends Component {
                     </div>
                 </div>
                 <div class="row" style={{ maxWidth: "1919px" }}>
-                    <div class="col-4" style={{ padding: "150px 75px 100px 150px" }}>
+                    <div class="col-4" style={{ padding: "120px 75px 100px 150px" }}>
                         <Jumbotron>
                             <h3>Workspace</h3>
                             <p></p>
@@ -42,14 +47,18 @@ export default class About extends Component {
                         </Jumbotron>
                     </div>
                     <div class="col-8" style={{ padding: "100px 100px 100px 100px" }}>
-                        <img src={workspaceImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                        <figure>
+                            <img src={workspaceImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                        </figure>
                     </div>
                 </div>
                 <div class="row" style={{ backgroundColor: "#F5F5F5", maxWidth: "1919px" }}>
                     <div class="col-8" style={{ padding: "100px 100px 100px 100px" }}>
-                        <img src={analyseFileImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                        <figure>
+                            <img src={analyseFileImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                        </figure>
                     </div>
-                    <div class="col-4" style={{ padding: "150px 150px 100px 75px" }}>
+                    <div class="col-4" style={{ padding: "108px 150px 100px 75px" }}>
                         <Jumbotron>
                             <h3>Analyse File</h3>
                             <p></p>
@@ -58,6 +67,36 @@ export default class About extends Component {
                             <p>With our simple and interactive drag & drop tecnology you can customize our analysis in any way you want!</p>
                             <p>You can even save the new changes and download the new and improved analysis, don't worry, if you save or change anything by accident we always keep track of the versions.</p>
                         </Jumbotron>
+                    </div>
+                </div>
+                <div class="row" style={{maxWidth: "1919px" }}>
+                    <div class="col-4" style={{ padding: "150px 150px 100px 75px" }}>
+                        <Jumbotron>
+                            <h3>About us</h3>
+                            <p></p>
+                            <p>This App was developed by three soon to be (we hope) engineers.</p>
+                            <p>It served as our final project to complete our bachelor's degree in Computed Engineering for ISEL.</p>
+                            <p>We learned alot from making this App and we really hope you like it!</p>
+                            <p>You can find the source code for this app in our public <a href="https://github.com/DuarteFelicio/DataAnnotation">github</a>.</p>
+                        </Jumbotron>
+                    </div>
+                    <div class="col-3" style={{ padding: "100px 100px 100px 100px" }}>
+                        <figure>
+                            <img src={duarteImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                            <figcaption>Duarte Felicio - Team Leader</figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-3" style={{ padding: "100px 100px 100px 100px" }}>
+                        <figure>
+                            <img src={cafeImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                            <figcaption>Ruben Café - Lead frontend developer</figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-2" style={{ padding: "100px 100px 100px 100px" }}>
+                        <figure>
+                            <img src={ivoImage} style={{ height: "100%", width: "100%", objectFit: "contain", border: '5px solid #333333' }} />
+                            <figcaption>Ivo Pereira - Lead backend developer</figcaption>
+                        </figure>
                     </div>
                 </div>
             </div>
